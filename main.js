7cc0b9b3935b5319653c37b3a8bfe5baabbdc71f
Michelle Beshears <meeshysu@gmail.com>
2018-09-21 18:36:46 -0500

    by the grace of the coding gods i figured out how to get that damn card to display

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const printToDom = (stringToPrint, whereToPrint) => {
   document.getElementById(whereToPrint).innerHTML = stringToPrint;
 };
 
+const letsStartElem = document.getElementById('letsStartBtn');
 const houses = ['Hufflepuff', 'Ravenclaw', 'Gryffindor', 'Slytherin'];
 let counter = 0;
 
@@ -11,19 +12,21 @@ const sortingButton = document.getElementById('letsStartBtn');
 sortingButton.addEventListener('click', () => {
   let newString =
     `<div class="form-group">
-          <label for="exampleInputEmail1">Enter Your Name Here:</label>
+          <label for="studentsName">Enter Your Name Here:</label>
           <input type="text" class="form-control" id="muggle" placeholder="Student Name">
     </div>
-          <button type="submit" id="begin" class="btn btn-primary id="sortingHat">Submit</button>
+          <button type="submit" id="sortingHat" class="btn btn-primary">Submit</button>
     </form>`;
   printToDom(newString, 'form');
 });
 
 
 const studentCardElem = () => {
+  //student's name:
  const muggleInput = document.getElementById('muggle').value;
+ //student's houses by random:
   const whichHouse = houses[Math.floor(Math.random()*houses.length)];
-    let newString = `<div class="card w-25 m-2 ">
+    let newString = `<div class="card w-25 m-2">
     <div class="card-body">
         <h5 class="card-title">${muggleInput}</h5>
         <p class="card-text">${whichHouse}</p>
@@ -34,14 +37,19 @@ counter++;
 printToDom(newString, 'card');
 };
 
-const studentsFutureHouse = () => {
-  const runningOutOfNameIdeas = document.getElementById('sortingHat');
-  runningOutOfNameIdeas.addEventListener('click', (e) => {
-    e.preventDefault();
+
+const studentCardBody = () => {
+  const sortingHatButtonElem = document.getElementById('sortingHat');
+  sortingHatButtonElem.addEventListener('click', (event) => {
+    event.preventDefault();
     studentCardElem();
-  }
-  )
+  });
 }
+
+letsStartElem.addEventListener('click', (event) => {
+  event.preventDefault();
+  studentCardBody();
+})
 
 
 //   // function showForm () {
