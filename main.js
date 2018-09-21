4c93a4c1ec35ca9552e259cf93721d005c4ac82c
Michelle Beshears <meeshysu@gmail.com>
2018-09-21 11:16:24 -0500

    trying to get the cards to show, still

--- main.js
+++ main.js
@@ -3,65 +3,50 @@ const printToDom = (stringToPrint, whereToPrint) => {
   document.getElementById(whereToPrint).innerHTML = stringToPrint;
 };
 
-const houses = [
-  'Hufflepuff',
-  'Ravenclaw',
-  'Gryffindor',
-  'Slytherin'
-];
+const houses = ['Hufflepuff', 'Ravenclaw', 'Gryffindor', 'Slytherin'];
+let counter = 0;
 
-const sortingButton = document.getElementById("letsStartBtn");
 
+const sortingButton = document.getElementById('letsStartBtn');
 sortingButton.addEventListener('click', () => {
-        let domString = `<div class="form-group">
-          <label for="exampleInputEmail1">StudentName</label>
-          <input type="text" class="form-control" id="stendt-name" aria-describedby="emailHelp" placeholder="Student Name">
-        </div>
-        <button type="submit" id="begin" class="btn btn-primary">Submit</button>
-      </form>`;
-        printToDom(domString, 'sortingForm'); 
+  let newString =
+    `<div class="form-group">
+          <label for="exampleInputEmail1">Enter Your Name Here:</label>
+          <input type="text" class="form-control" id="muggle" placeholder="Student Name">
+    </div>
+          <button type="submit" id="begin" class="btn btn-primary id="sortingHat">Submit</button>
+    </form>`;
+  printToDom(newString, 'form');
 });
-const sortingButton2 = document.getElementById("sortingForm");
 
-sortingButton2.addEventListener('click', () => {
-  let domString = `<div class="card" style="width: 18rem;">
 
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>`;
-  if (stendt-name === houses[0]) {
+const studentCardElem = () => {
+ const muggleInput = document.getElementById('muggle').value;
+  const whichHouse = houses[Math.floor(Math.random()*houses.length)];
+    let newString = `<div class="card w-25 m-2 ">
+    <div class="card-body">
+        <h5 class="card-title">${muggleInput}</h5>
+        <p class="card-text">${whichHouse}</p>
+        <button href="#" class="btn btn-danger expelButton">Expel</button>
+    </div>
+  </div>`;
+counter++;
+printToDom(newString, 'card');
+};
 
+const studentsFutureHouse = () => {
+  const runningOutOfNameIdeas = document.getElementById('sortingHat');
+  runningOutOfNameIdeas.addEventListener('click', (e) => {
+    e.preventDefault();
+    studentCardElem();
   }
-printToDom(domString, 'sortingCardBegin');
-});
+  )
+}
 
-// let outputHouse = '';
-// const houseDecider = (whichHouse) => {
-//   outputHouse = ' ';
-//   let inputHouse = document.getElementById('sortingCardBegin').value.toLowerCase();
-//   for (let i = 0; i <inputHouse.length; i++) {
-//     outputHouse += `${houses[inputHouses[i]]}`;
-//     if (houses === inputHouses[i]) {
-//       printToDom(domString, 'sortingCardBegin');
-//     }
-//   }
-// }
-
-// /* <img class="card-img-top" src=".../100px180/" alt="Card image cap"> */
 
 //   // function showForm () {
 //   //   document.getElementById('submit').style.display="block";
 //   // };
-
-
-
-
-
-
-sortingButton2 = Math.floor(Math.random()*4);
-  if (sortingButton2 === [0]) {
-    printToDom(houses, 'sortingCardBegin');
-  }
+// const studentCardElem = document.getElementById('');
+// studentCardElem.addEventListener('click', () => {
+//   const studentName = document.getElementById('muggle').value;
