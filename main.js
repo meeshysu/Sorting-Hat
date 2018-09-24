95bb16179d5d127bd1b425773a3cb323c958bf64
Michelle Beshears <meeshysu@gmail.com>
2018-09-24 16:08:31 -0500

    cleaned code

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let counter = 0;
 const sortingButton = document.getElementById('letsStartBtn');
 sortingButton.addEventListener('click', () => {
   let newString =
-  `<div id="imTired" class="form-group-one">
+    `<div id="imTired" class="form-group-one">
           <label for="studentsName">Enter Your Name Here:</label>
           <input type="text" class="form-control w-20" id="muggle" placeholder="Student Name">
           <button type="submit" id="sortingHat" class="d-inline-flex btn btn-warning w-20">Submit</button>
@@ -22,18 +22,18 @@ sortingButton.addEventListener('click', () => {
 
 
 const studentCardElemBuilder = () => {
- const muggleInput = document.getElementById('muggle').value;
-  const whichHouse = houses[Math.floor(Math.random()*houses.length)];
-    let newString = `<div class="card w-25" id ="thisWorks">
+  const muggleInput = document.getElementById('muggle').value;
+  const whichHouse = houses[Math.floor(Math.random() * houses.length)];
+  let newString = `<div class="card w-25" id ="thisWorks">
     <div class="card-body center-block">
         <h5 class="card-title">${muggleInput}</h5>
         <p class="card-text">${whichHouse}</p>
         <button id="expel" class="btn btn-danger expelButton">Expel</button>
     </div>
   </div>`;
-counter++;
-printToDom(newString, 'card');
-activateExpels();
+  counter++;
+  printToDom(newString, 'card');
+  activateExpels();
 };
 
 const studentCardBody = () => {
@@ -41,7 +41,7 @@ const studentCardBody = () => {
   sortingHatButtonElem.addEventListener('click', (event) => {
     event.preventDefault();
     studentCardElemBuilder();
- 
+
   });
 }
 
@@ -63,11 +63,11 @@ letsStartElem.addEventListener('click', (event) => {
 // }
 // }
 const activateExpels = () => {
-document.querySelector("body").addEventListener("click", function(event) {
+  document.querySelector("body").addEventListener("click", function (event) {
     if (event.target.classList.contains('expelButton')) {
       const buttonIClicked = event.target;
-        const cardToDelete = buttonIClicked.parentNode.parentNode;
-        cardToDelete.remove();
+      const cardToDelete = buttonIClicked.parentNode.parentNode;
+      cardToDelete.remove();
     }
   })
 }
